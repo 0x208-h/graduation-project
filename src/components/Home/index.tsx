35625a8a22da2c4cb6c1e1135add9c491104d49c
“hui1.huang” <hui1.huang@ximalaya.com>
2022-03-01 19:17:19 +0800

    测试路由

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Link, Outlet } from 'react-router-dom'
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import styles from './index.module.scss'
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const Home = () => {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+
+  const onCollapse = () => setCollapsed(!collapsed);
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse} className={styles.container}>
+        <div className={styles.logo} />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+          <Link to="./user">用户管理</Link>
+            
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            <Link to="./good">商品管理</Link>
+          </Menu.Item>
+          {/* <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu> */}
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className={styles.siteLayout}>
+        <Header className={styles.siteLayoutBackground} />
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+          <Outlet />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          E-commerce Background Management System ©2022 Created by HCH
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Home;
--- src/route.tsx
+++ src/route.tsx
@@ -1,13 +1,38 @@
 import { lazy } from "react";
 import { useRoutes } from "react-router-dom";
 
-const Login = lazy(() => import(/*WebpackChunkName: Login */ "@/components/Login"));
+const Login = lazy(
+  () => import(/*WebpackChunkName: Login */ "@/components/Login")
+);
+const Home = lazy(
+  () => import(/*WebpackChunkName: Home */ "@/components/Home")
+);
+const Users = lazy(
+  () => import(/*WebpackChunkName: Users */ "@/components/Users")
+);
+const Goods = lazy(
+  () => import(/*WebpackChunkName: Goods */ "@/components/Goods")
+);
 
 const useRoute = () => {
   return useRoutes([
     {
-      path: '/',
-      element: <Login />
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+      children: [
+        {
+          path: "user",
+          element: <Users />,
+        },
+        {
+          path: "good",
+          element: <Goods />,
+        },
+      ],
     },
   ]);
 };
